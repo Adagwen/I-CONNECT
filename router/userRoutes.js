9a9cd94fd696869896d302383f077195b4fb72cf
Adagwen <ezeokaforadaobee@gmail.com>
2024-04-05 14:36:14 +0100

    user

--- router/userRoutes.js
+++ router/userRoutes.js
@@ -14,10 +14,10 @@ const validateToken = require("../middleware/validateToken");
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/currentUser", validateToken, currentUser);
-router.get("/getUsers",  getUsers);
+router.get("/allUsers",  getUsers);
 router.get("/:id",  getUserById);
-router.get("/:id",  updateUser);
-router.get("/:id",  deleteUser);
+router.put("/:id",  updateUser);
+router.delete("/:id",  deleteUser);
 
 
 
